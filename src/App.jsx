64ada8cd481ba9ec704466d992a038c7a1638f38
chamuKP <chamucreation000@gmail.com>
2024-11-19 09:19:11 +0000

    commit1

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,19 @@
+import { useState } from 'react';
 import './App.css';
+import Childcom from './Childcom';
+
 
 function App() {
+
+  const [childname,setChildname]=useState("Default");
+  const receiveName=(name)=>(
+    setChildname(name)
+  )
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+      <h2>Hello React I'm Parent</h2>
+      <Childcom setfun={receiveName}/>
+      <P>Parent: My child said: {childname}</P>
     </div>
   );
 }
